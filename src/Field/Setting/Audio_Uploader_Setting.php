eb7b65fbce6970aeab55e20ab3d66f4d81f8e9d8
Luke Watts <watts.luke@yahoo.com>
2024-05-07 12:38:25 +0100

    Completed v1 of JSON schemas

--- src/Field/Setting/Audio_Uploader_Setting.php
+++ src/Field/Setting/Audio_Uploader_Setting.php
@@ -14,5 +14,5 @@ class Audio_Uploader_Setting extends Uploader_Setting
 {
     protected string $type = 'audio-uploader';
 
-    protected array $valid_extensions = ['aac', 'aiff', 'alac', 'flac', 'mp3', 'ogg', 'wav', 'wma'];
+    protected array $valid_extensions = ['ogg', 'm4a', 'mp3', 'wav'];
 }
